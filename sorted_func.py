54c0c901fcee677a59e512b8569e5816a8fcb67f
vlad3216 <103311342+vlad3216@users.noreply.github.com>
2022-05-13 15:34:43 +0300

    Create sorted_func.py

---
+++ sorted_func.py
@@ -0,0 +1,6 @@
+def sorted(list):
+   if print != list:
+      for i in range(len(list)):
+          for c in range(i + 1, len(list)):
+            if list[i] > list[c]:
+             list[i], list[c] = list[c],list[i]
